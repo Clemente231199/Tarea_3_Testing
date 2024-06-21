2c881c5abfe3ce619851b88271b976096ef02820
clemente231199 <clemente231199@LAPTOP-2Q9UM16P>
2024-06-21 12:04:21 -0400

    System test forms

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Aníbal Trabajo en el setup, github actions y completar los requests.
 Corriendo con simple coverage, el html entrega 100% en modelos y casi todos los requests.
 
 Para products_controller, se testea el camino en que el current_user no es admin para los metodos actualizar_producto y eliminar, en ests caso no se llega al else "Debes ser un administrador para modificar un producto." sino que hacer el request entrega "No estás autorizado para acceder a esta página". Esta es la ruta que se testea.
+
+Se ha cambiado la dirección que se reenvia al apretar el boton "Cancelar" desde la creación de un producto. En vez de reenviar a url_for(:back), se reenvia al index.
--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -14,6 +14,213 @@ RSpec.describe 'Products', type: :system do
       expect(page).to have_selector('h1', text: 'Crear Producto')
     end
 
+    it 'creates a cancha' do
+      visit '/products/crear'
+
+      fill_in 'Nombre', with: 'Primera cancha'
+
+      fill_in 'Precio', with: '1000'
+
+      fill_in 'Stock', with: '15'
+
+      fill_in 'Horarios', with: '12,13,14'
+
+      click_on 'Guardar'
+
+      expect(page).to have_content('Producto creado Correctamente !')
+    end
+
+    it 'create a cancha but stock negative' do
+      visit '/products/crear'
+
+      fill_in 'Nombre', with: 'Primera cancha'
+
+      fill_in 'Precio', with: '1000'
+
+      fill_in 'Stock', with: '-15'
+
+      fill_in 'Horarios', with: '12,13,14'
+
+      click_on 'Guardar'
+
+      expect(page).to have_content("Hubo un error al guardar el producto: Stock: debe ser mayor que o igual a 0")
+
+    end
+
+    it 'create a cancha but price negative' do
+      visit '/products/crear'
+
+      fill_in 'Nombre', with: 'Primera cancha'
+
+      fill_in 'Precio', with: '-1000'
+
+      fill_in 'Stock', with: '15'
+
+      fill_in 'Horarios', with: '12,13,14'
+
+      click_on 'Guardar'
+
+      expect(page).to have_content("Hubo un error al guardar el producto: Precio: debe ser mayor que o igual a 0")
+
+    end
+
+    it 'create a cancha but price and stock negative' do
+      visit '/products/crear'
+
+      fill_in 'Nombre', with: 'Primera cancha'
+
+      fill_in 'Precio', with: '-1000'
+
+      fill_in 'Stock', with: '-15'
+
+      fill_in 'Horarios', with: '12,13,14'
+
+      click_on 'Guardar'
+
+      expect(page).to have_content("Hubo un error al guardar el producto: Stock: debe ser mayor que o igual a 0, Precio: debe ser mayor que o igual a 0")
+
+    end
+
+    it 'cancel the creation of a product' do
+      visit '/products/crear'
+
+      fill_in 'Nombre', with: 'Primera cancha'
+
+      fill_in 'Precio', with: '-1000'
+
+      fill_in 'Stock', with: '-15'
+
+      fill_in 'Horarios', with: '12,13,14'
+
+      click_on 'Cancelar'
+
+      expect(page).to have_current_path('/products/index')
+
+    end
+
+
+
+    it 'update cancha' do
+      visit '/products/crear'
+
+      fill_in 'Nombre', with: 'Primera cancha'
+
+      fill_in 'Precio', with: '1000'
+
+      fill_in 'Stock', with: '15'
+
+      fill_in 'Horarios', with: '12,13,14'
+
+      click_on 'Guardar'
+    
+      visit '/products/index'
+
+      expect(page).to have_css('.card')
+    
+      within(".card", match: :first) do
+        click_on 'Editar'
+      end
+    
+      fill_in 'Nombre', with: 'Primera cancha actualizada'
+      fill_in 'Precio', with: '2000'
+      fill_in 'Stock', with: '20'
+    
+      click_on 'Guardar' 
+    
+
+      cancha = Product.order(created_at: :desc).first
+    
+      expect(cancha.nombre).to eq('Primera cancha actualizada')
+      expect(cancha.precio).to eq("2000")
+      expect(cancha.stock).to eq("20")
+    end
+
+    it 'update cancha but no name' do
+      visit '/products/crear'
+
+      fill_in 'Nombre', with: 'Primera cancha'
+
+      fill_in 'Precio', with: '1000'
+
+      fill_in 'Stock', with: '15'
+
+      fill_in 'Horarios', with: '12,13,14'
+
+      click_on 'Guardar'
+    
+      visit '/products/index'
+
+      expect(page).to have_css('.card')
+    
+      within(".card", match: :first) do
+        click_on 'Editar'
+      end
+    
+      fill_in 'Nombre', with: ''
+    
+      click_on 'Guardar' 
+    
+      expect(page).to have_content('Hubo un error al guardar el producto. Complete todos los campos solicitados!')
+    end
+
+    it 'update cancha but negative stock' do
+      visit '/products/crear'
+
+      fill_in 'Nombre', with: 'Primera cancha'
+
+      fill_in 'Precio', with: '1000'
+
+      fill_in 'Stock', with: '15'
+
+      fill_in 'Horarios', with: '12,13,14'
+
+      click_on 'Guardar'
+    
+      visit '/products/index'
+
+      expect(page).to have_css('.card')
+    
+      within(".card", match: :first) do
+        click_on 'Editar'
+      end
+    
+      fill_in 'Stock', with: '-20'
+    
+      click_on 'Guardar' 
+    
+      expect(page).to have_content('Hubo un error al guardar el producto. Complete todos los campos solicitados!')
+    end
+
+    it 'update cancha but cancel' do
+      visit '/products/crear'
+
+      fill_in 'Nombre', with: 'Primera cancha'
+
+      fill_in 'Precio', with: '1000'
+
+      fill_in 'Stock', with: '15'
+
+      fill_in 'Horarios', with: '12,13,14'
+
+      click_on 'Guardar'
+    
+      visit '/products/index'
+
+      expect(page).to have_css('.card')
+    
+      within(".card", match: :first) do
+        click_on 'Editar'
+      end
+    
+    
+      click_on 'Cancelar'
+
+      expect(page).to have_current_path('/products/index')
+    end
+
+
+    
+    
     it 'not allowed on the product form' do
       logout(:user)
       visit '/products/crear'
